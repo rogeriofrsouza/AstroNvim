c00faf6e92b51b7103c6d4972c6f8d07739ce135
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-04-20 13:24:34 -0300

    feat(community.lua): add community lang packs

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,4 +7,9 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.angular" },
 }
